51cd5e50a8f9ed3b79065580c91213e7a9d7fdd9
jakebark <jacob.barker@gmail.com>
2025-06-01 20:24:12 +0100

    add config

--- lua/notes.lua
+++ lua/notes.lua
@@ -1,7 +1,21 @@
 local M = {}
 
+-- default config
+local config = {
+    height = 20,
+    width = 80,
+    relative_numbers = true,                                -- false for absolute
+    notes_file_path = vim.fn.expand("$HOME") .. "/notes.md" -- default notes file path
+}
+
+-- overide default config
+function M.setup(user_config)
+    user_config = user_config or {}
+    config = vim.tbl_deep_extend("force", config, user_config)
+end
+
 function M.open_notes()
-    local notes_file = vim.fn.expand("$HOME") .. "/notes.md"
+    local notes_file = config.notes_file_path
 
     if not vim.fn.filereadable(notes_file) then
         vim.fn.writefile({}, notes_file)
@@ -29,21 +43,23 @@ function M.open_notes()
     vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
     vim.api.nvim_buf_set_option(buf, "buftype", "")
 
-    local width = 80
-    local height = 20
-
     local win = vim.api.nvim_open_win(buf, true, {
         relative = 'editor',
         row = math.floor((vim.o.lines - height) / 2),
         col = math.floor((vim.o.columns - width) / 2),
-        width = width,
-        height = height,
+        width = config.width,
+        height = config.width,
         style = 'minimal',
         border = 'single',
     })
 
-    vim.api.nvim_buf_set_option(buf, 'number', true)
-    vim.api.nvim_buf_set_option(buf, 'relativenumber', true)
+    if config.relative_numbers then
+        vim.api.nvim_buf_set_option(buf, 'relativenumber', true)
+        vim.api.nvim_buf_set_option(buf, 'number', true)
+    else
+        vim.api.nvim_buf_set_option(buf, 'number', true)
+        vim.api.nvim_buf_set_option(buf, 'relativenumber', false)
+    end
 
     -- focus on floating window
     vim.api.nvim_set_current_win(win)
